eb07008d9d06d22c8d971826766437af11760cc6
Alex Muthui <alexmuthui35@gmail.com>
2019-01-25 18:02:00 +0300

    First Commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_22_110653) do
+ActiveRecord::Schema.define(version: 2019_01_25_040832) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "name"
+    t.text "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "post_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
